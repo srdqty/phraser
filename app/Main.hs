aa24c1dbdb08a947b73aa7daba0848f4bf38298a
Sophia Donataccio <srdqty@gmail.com>
2017-06-09 02:37:36 -0400

    Add initial proof-of-concept

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,26 @@
 module Main where
 
-import DicewarePhrase.WordList (wordList)
+import Control.Monad.State (evalStateT, replicateM, StateT, get, put)
 import qualified Data.Map as Map (lookup)
 import Data.Maybe (fromJust)
+import Crypto.Random (getSystemDRG, randomBytesGenerate, SystemDRG)
+import Data.ByteArray (unpack, Bytes)
+
+import DicewarePhrase.WordList (wordList)
+
+diceToInt :: Integral a => [a] -> a
+diceToInt ns = sum (zipWith (*) ns [1, 10, 100, 1000, 10000])
 
 main :: IO ()
-main = putStrLn (fromJust $ Map.lookup 11111 wordList)
+main = getSystemDRG >>= evalStateT genPhrase >>= putStrLn
+
+genPhrase :: StateT SystemDRG IO String
+genPhrase = unwords <$> replicateM 10 genWord
+    where
+        genWord :: StateT SystemDRG IO String
+        genWord = do
+            drg <- get
+            let (ns, drg') = randomBytesGenerate 5 drg :: (Bytes, SystemDRG)
+            put drg'
+            let n = diceToInt ((+1) . round . (*5) . (/255) . toRational <$> unpack ns)
+            return (fromJust $ Map.lookup n wordList)
